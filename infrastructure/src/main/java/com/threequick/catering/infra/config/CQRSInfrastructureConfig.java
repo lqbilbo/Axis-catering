d7b63ffdcc1bea1a12e3c0dd520dfa46e4485801
罗琦 <luoqi04@meituan.com>
2018-07-27 06:55:05 +0800

    add CQRS config & modify pom

---
+++ infrastructure/src/main/java/com/threequick/catering/infra/config/CQRSInfrastructureConfig.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2010-2016. Axon Framework
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.threequick.catering.infra.config;
+
+import net.sf.ehcache.CacheManager;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
+import org.axonframework.common.caching.EhCacheAdapter;
+import org.axonframework.messaging.interceptors.BeanValidationInterceptor;
+import org.axonframework.spring.config.CommandHandlerSubscriber;
+import org.axonframework.spring.config.annotation.AnnotationCommandHandlerBeanPostProcessor;
+import org.axonframework.spring.eventsourcing.SpringAggregateSnapshotterFactoryBean;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan("com.threequick.catering")
+public class CQRSInfrastructureConfig {
+
+    @Bean
+    public CommandBus commandBus() {
+        SimpleCommandBus commandBus = new SimpleCommandBus();
+        commandBus.registerDispatchInterceptor(new BeanValidationInterceptor<>());
+
+        return commandBus;
+    }
+
+    @Bean
+    public CommandGateway commandGateway(CommandBus commandBus) {
+        return new DefaultCommandGateway(commandBus);
+    }
+
+    @Bean
+    public AnnotationCommandHandlerBeanPostProcessor annotationCommandHandlerBeanPostProcessor() {
+        return new AnnotationCommandHandlerBeanPostProcessor();
+    }
+
+    @Bean
+    public CommandHandlerSubscriber commandHandlerSubscriber() {
+        return new CommandHandlerSubscriber();
+    }
+
+    @Bean
+    public SpringAggregateSnapshotterFactoryBean springAggregateSnapshotterFactoryBean() {
+        return new SpringAggregateSnapshotterFactoryBean();
+    }
+
+    @Bean
+    public EhCacheAdapter ehCache(CacheManager cacheManager) {
+        return new EhCacheAdapter(cacheManager.getCache("testCache"));
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setShared(true);
+
+        return ehCacheManagerFactoryBean;
+    }
+}
--- web/src/main/java/com/threequick/config/RootConfig.java
+++ web/src/main/java/com/threequick/config/RootConfig.java
@@ -1,12 +1,11 @@
 package com.threequick.config;
 
+import com.threequick.catering.infra.config.CQRSInfrastructureConfig;
 import com.threequick.catering.infra.config.PersistenceInfrastructureConfig;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(basePackages = "com.threequick.catering")
-@Import({PersistenceInfrastructureConfig.class})
+@Import({CQRSInfrastructureConfig.class, PersistenceInfrastructureConfig.class})
 public class RootConfig {
 }
