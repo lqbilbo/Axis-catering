4822d0ca911247cbaecfad9896219b4a2cd4c7f2
罗琦 <luoqi04@meituan.com>
2018-08-07 22:49:54 +0800

    add stall commands

--- api/src/main/java/com/threequick/catering/api/kds/stall/commands.kt
+++ api/src/main/java/com/threequick/catering/api/kds/stall/commands.kt
@@ -1,2 +1,18 @@
 package com.threequick.catering.api.kds.stall
 
+import org.axonframework.commandhandling.TargetAggregateIdentifier
+
+abstract class StallCommand(@TargetAggregateIdentifier open val stallId: StallId)
+
+data class CreateStallCommand(
+        override val stallId: StallId,
+        val poiId: Long,
+        val stallName: String,
+        val remark: String,
+        val requirements: String,
+        val ability: Int
+) : StallCommand(stallId)
+
+data class DeleteStallCommand(
+        override val stallId: StallId
+) : StallCommand(stallId)
