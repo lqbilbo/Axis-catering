50ae9b45d8b76e43d68dd95d75a9f4fb383f6e13
罗琦 <luoqi04@meituan.com>
2018-08-12 23:56:16 +0800

    add servery core-api & aggragate commands+events

--- api/src/main/java/com/threequick/catering/api/kds/servery/events.kt
+++ api/src/main/java/com/threequick/catering/api/kds/servery/events.kt
@@ -1,2 +1,37 @@
 package com.threequick.catering.api.kds.servery
 
+import com.threequick.catering.api.kds.stall.StallId
+
+abstract class ServeryEvent(open val serveryId: ServeryId)
+
+data class ServeryCreatedEvent(
+        override val serveryId: ServeryId,
+        val poiId: Long,
+        val serveryName: String,
+        val remark: String,
+        val online: Boolean,
+        val offline: Boolean
+) : ServeryEvent(serveryId)
+
+data class ServeryUpdatedEvent(
+        val serveryName: String,
+        val remark: String,
+        val online: Boolean,
+        val offline: Boolean
+)
+
+data class StallsAddedToServeryEvent(
+        override val serveryId: ServeryId,
+        val stallId: StallId,
+        val amountOfStallsToAdd: Long
+) : ServeryEvent(serveryId)
+
+data class StallsRemovedFromServeryEvent(
+        override val serveryId: ServeryId,
+        val stallId: StallId
+) : ServeryEvent(serveryId)
+
+data class ServeryDeletedEvent(
+        override val serveryId: ServeryId
+) : ServeryEvent(serveryId)
+
--- kds/src/main/java/com/threequick/catering/kds/config/Servery.java
+++ kds/src/main/java/com/threequick/catering/kds/config/Servery.java
@@ -1,4 +1,66 @@
 package com.threequick.catering.kds.config;
 
+import com.threequick.catering.api.kds.servery.*;
+import com.threequick.catering.api.kds.stall.StallId;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
+
+@Aggregate(repository = "serveryAggregateRepository")
 public class Servery {
+
+    private static final long DEFAULT_STALL_VALUE = 0L;
+
+    @AggregateIdentifier
+    private ServeryId serveryId;
+    private Map<StallId, Long> availableItems;
+    private Map<StallId, Long> reservedItems;
+
+    public Servery() {
+    }
+
+    @CommandHandler
+    public Servery(CreateServeryCommand cmd) {
+        apply(new ServeryCreatedEvent(cmd.getServeryId(), cmd.getPoiId(), cmd.getServeryName(),
+                cmd.getRemark(), cmd.getOnline(), cmd.getOffline()));
+    }
+
+    @CommandHandler
+    public Servery(AddStallsToServeryCommand cmd) {
+        apply(new StallsAddedToServeryEvent(serveryId, cmd.getStallId(), cmd.getAmountOfStallsToAdd()));
+    }
+
+    @CommandHandler
+    public Servery(RemoveStallsToServeryCommand cmd) {
+        apply(new StallsRemovedFromServeryEvent(serveryId, cmd.getStallId()));
+    }
+
+    @EventSourcingHandler
+    public void on(ServeryCreatedEvent event) {
+        serveryId = event.getServeryId();
+        availableItems = new HashMap<>();
+        reservedItems = new HashMap<>();
+    }
+
+    @EventSourcingHandler
+    public void on(StallsAddedToServeryEvent event) {
+        long available = obtainCurrentAvailableItems(event.getStallId());
+        availableItems.put(event.getStallId(), available + event.getAmountOfStallsToAdd());
+    }
+
+    @EventSourcingHandler
+    public void on(StallsRemovedFromServeryEvent event) {
+        availableItems.put(event.getStallId(), DEFAULT_STALL_VALUE);
+    }
+
+    private long obtainCurrentAvailableItems(StallId stallId) {
+        return availableItems.getOrDefault(stallId, DEFAULT_STALL_VALUE);
+    }
+
 }
